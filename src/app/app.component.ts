4da33994404370af3780ff1b73799d04222deffe
Eliana88 <eliana.ramos1988@gmail.com>
2020-05-19 13:42:02 -0300

    primera parte

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+/*   template: '<h1> Bienvenidos a Angular</h1><h2>Como va?</h2>',
+  styles: ['h1 {color: blue', 'h2 {color:red}'] */
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
